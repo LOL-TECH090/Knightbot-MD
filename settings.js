06df8fb736318b0d73db2a5c57fd0daff0b6f91a
LOL-TECH090 <dorandfinlay@gmail.com>
2025-04-22 22:30:53 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
-  author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  packname: 'LOL-TECH',
+  author: 'LOLMANN_090',
+  botName: "LOL-TECH BOT",
+  botOwner: 'LOLMANN_090', // Your name
+  ownerNumber: '4917681346302', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
